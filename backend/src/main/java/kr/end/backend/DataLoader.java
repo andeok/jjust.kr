291da26a6e8fcc4afaab0c69992e7a0795a1a1ba
dg.an <dg.an@efit.co.kr>
2025-05-20 17:11:28 +0900

    feat: 회원가입 처리

--- backend/src/main/java/kr/end/backend/DataLoader.java
+++ backend/src/main/java/kr/end/backend/DataLoader.java
@@ -1,7 +1,7 @@
 package kr.end.backend;
 
 import kr.end.backend.auth.dto.request.SignupRequest;
-import kr.end.backend.member.service.MemberService;
+import kr.end.backend.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -14,15 +14,15 @@ import org.springframework.stereotype.Component;
 @Profile("!prod")
 public class DataLoader implements CommandLineRunner {
 
-    private final MemberService memberService;
+  private final AuthService authService;
 
-    @Override
-    public void run(String... args) throws Exception {
-        SignupRequest test1 = new SignupRequest("test1@naver.com", "1234", "test1");
-        memberService.signupMember(test1);
+  @Override
+  public void run(String... args) throws Exception {
+    SignupRequest test1 = new SignupRequest("test1@naver.com", "1234", "test1");
+    authService.signupMember(test1);
 
-        SignupRequest test2 = new SignupRequest("test2@naver.com", "1234", "test2");
-        memberService.signupMember(test2);
+    SignupRequest test2 = new SignupRequest("test2@naver.com", "1234", "test2");
+    authService.signupMember(test2);
 
-    }
+  }
 }
--- backend/src/main/java/kr/end/backend/auth/dto/request/SignupRequest.java
+++ backend/src/main/java/kr/end/backend/auth/dto/request/SignupRequest.java
@@ -1,18 +1,27 @@
 package kr.end.backend.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import kr.end.backend.member.domain.Member;
 
 public record SignupRequest(
-        String email,
-        String password,
-        String nickname
+
+    @Email
+    @NotBlank(message = "이메일을 입력하세요.")
+    String email,
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    String password,
+
+    @NotBlank(message = "닉네임을 입력하세요.")
+    String nickname
 ) {
 
-    public Member toEntity(SignupRequest request, String encode) {
-        return Member.builder()
-                .email(request.email())
-                .password(encode)
-                .nickname(request.nickname())
-                .build();
-    }
+  public Member toEntity(SignupRequest request, String encode) {
+    return Member.builder()
+        .email(request.email())
+        .password(encode)
+        .nickname(request.nickname())
+        .build();
+  }
 }
