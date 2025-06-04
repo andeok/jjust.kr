57e6dbf0e5d5d413c7f7a5e604d153f45638478b
Deokgyun an <97350241+andeok@users.noreply.github.com>
2025-06-04 17:32:11 +0900

    feat: swagger 컨트롤러 분리 / 물품 관련 메소드 추가 (#35)
    - 판매액 등록 시 차액 계산
    - 판매날짜 등록 시 사용일수 및 일일 비용 계산
    
    Co-authored-by: dg.an <dg.an@efit.co.kr>

---
+++ backend/src/main/java/kr/end/backend/item/controller/ItemSwaggerController.java
@@ -0,0 +1,42 @@
+package kr.end.backend.item.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import kr.end.backend.item.dto.request.ItemRequest;
+import kr.end.backend.item.dto.response.ItemResponse;
+import kr.end.backend.member.domain.Member;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "물품 관련 API")
+public interface ItemSwaggerController {
+
+  @Operation(summary = "물품 등록", responses = {
+      @ApiResponse(responseCode = "200", description = "물품 등록 성공",
+          content = @Content(schema = @Schema(implementation = ItemResponse.class)))})
+  public ResponseEntity<ItemResponse> createItem(@Parameter(hidden = true) Member member,
+      ItemRequest request);
+
+  @Operation(summary = "선택한 물품 조회", responses = {
+      @ApiResponse(responseCode = "200", description = "물품 조회 성공",
+          content = @Content(schema = @Schema(implementation = ItemResponse.class)))})
+  public ResponseEntity<ItemResponse> getItem(@Parameter(hidden = true) Member member, Long id);
+
+  @Operation(summary = "내 물품 전체 조회", responses = {
+      @ApiResponse(responseCode = "200", description = "물품 조회 성공",
+          content = @Content(schema = @Schema(implementation = ItemResponse[].class)))})
+  public ResponseEntity<List<ItemResponse>> getItems(@Parameter(hidden = true) Member member);
+
+  @Operation(summary = "선택한 물품 삭제", responses = {
+      @ApiResponse(responseCode = "200", description = "물품 삭제 성공")})
+  public ResponseEntity<Void> deleteItem(@Parameter(hidden = true) Member member, Long id);
+
+  @Operation(summary = "선택한 물품 수정", responses = {
+      @ApiResponse(responseCode = "200", description = "물품 수정 성공")})
+  public ResponseEntity<Void> updateItem(@Parameter(hidden = true) Member member, Long id,
+      ItemRequest request);
+}
