8702b308e0e02c190147e32cf66f50a122f0f968
Deokgyun an <97350241+andeok@users.noreply.github.com>
2025-06-04 11:31:17 +0900

    feat: swagger 추가 및 로그인 주소 변경 (#32) (#33)
    Co-authored-by: dg.an <dg.an@efit.co.kr>

---
+++ backend/src/main/java/kr/end/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package kr.end.backend.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    Info info = new Info()
+        .title("JJUST Backend API")
+        .version("1.0");
+
+    return new OpenAPI()
+        .components(new Components())
+        .info(info);
+  }
+
+  @Bean
+  public ForwardedHeaderFilter forwardedHeaderFilter() {
+    return new ForwardedHeaderFilter();
+  }
+}
---
+++ backend/src/main/java/kr/end/backend/member/controller/MemberSwaggerController.java
@@ -0,0 +1,19 @@
+package kr.end.backend.member.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.end.backend.auth.dto.response.MemberResponse;
+import kr.end.backend.member.domain.Member;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "MyInfo API")
+public interface MemberSwaggerController {
+
+  @Operation(summary = "내 정보 조회", responses = {
+      @ApiResponse(responseCode = "200", description = "내 정보 조회 성공", content = @Content(schema = @Schema(implementation = MemberResponse.class)))})
+  public ResponseEntity<MemberResponse> readUserInfo(@Parameter(hidden = true) Member member);
+}
